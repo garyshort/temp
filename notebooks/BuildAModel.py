e5eab3d40a7730e831661def5a8a9b1316662408
Gary Short <theothergaryshort@hotmail.co.uk>
2018-10-17 16:43:59 +0100

    Initial commit

---
+++ notebooks/BuildAModel.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+# Read the data from blob
+
+# COMMAND ----------
+
+# Clean the data
+
+# COMMAND ----------
+
+# Build a model
+
+# COMMAND ----------
+
+# Register the model 
